2afde4d18203eddc363014ddf05399991e69ddd2
pozafly <pozafly@gmail.com>
2023-08-15 14:13:22 +0900

    chore: lint-staged 설치 및 husky 연동

--- src/index.js
+++ src/index.js
@@ -5,6 +5,14 @@ import Apple from './assets/images/apple.png';
 
 const app = document.querySelector('#app');
 
+const lintTest = condition => {
+  if (condition) {
+    return 'a';
+  }
+  return 'b';
+};
+console.log(lintTest(true));
+
 const sampleImg = document.createElement('img');
 sampleImg.src = Apple;
 sampleImg.width = '304';
