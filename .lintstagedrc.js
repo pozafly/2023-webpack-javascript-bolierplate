1e0d46c9921aed59f4df18fdfd846542f8733435
pozafly <pozafly@gmail.com>
2024-02-06 23:11:21 +0900

    fix: lint-staged 모든 파일 대상으로 하지 않도록 수정

--- .lintstagedrc.js
+++ .lintstagedrc.js
@@ -1,3 +1,6 @@
 module.exports = {
-  'src/**/*.{js,jsx,ts,tsx}': ['npm run lint', 'npm run format'],
+  'src/**/*.{js,jsx,ts,tsx}': [
+    'eslint --fix --cache',
+    'prettier --write --cache',
+  ],
 };
